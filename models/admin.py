88423274c3fa53107dc48040d8e12acb942304ec
Enapiuz <enapiuz@gmail.com>
2015-07-21 00:16:27 +0300

    Реформат представления модели крана и служебных моделей

--- models/admin.py
+++ models/admin.py
@@ -2,18 +2,38 @@ from django.contrib import admin
 from .models import Wallet, WalletCategory, Faucet, MetaKeyword, Captcha, Currency, FaucetCategory
 
 
+class WalletCategoryAdmin(admin.ModelAdmin):
+    get_model_perms = lambda self, request: {}
+
+class FaucetCategoryAdmin(admin.ModelAdmin):
+    get_model_perms = lambda self, request: {}
+
+class CaptchaAdmin(admin.ModelAdmin):
+    get_model_perms = lambda self, request: {}
+
+class CurrencyAdmin(admin.ModelAdmin):
+    get_model_perms = lambda self, request: {}
+
+
 class FaucetAdmin(admin.ModelAdmin):
     """
     Пока заглушка, чтобы не забыть описание
     """
     fieldsets = [
-        ('Likes', {'fields': ['likes', 'dislikes']})
+        ('Основное', {'fields': ['title_en', 'title_ru', 'href']}),
+        ('Служебное',
+         {'fields': ['currency', 'now_pays', 'captcha', 'category', 'update_time', 'visible', 'malfunction']}),
+        ('Награда', {'fields': ['reward_min', 'reward_max', 'reward_mid']}),
+        ('Лайки', {'fields': ['likes', 'dislikes']}),
+        ('Meta', {'fields': ['meta_title', 'meta_description', 'meta_keywords']}),
+        (None, {'fields': ['views']})
     ]
 
+
 admin.site.register(Wallet)
-admin.site.register(WalletCategory)
-admin.site.register(Faucet)
-admin.site.register(FaucetCategory)
+admin.site.register(WalletCategory, WalletCategoryAdmin)
+admin.site.register(Faucet, FaucetAdmin)
+admin.site.register(FaucetCategory, FaucetCategoryAdmin)
 admin.site.register(MetaKeyword)
-admin.site.register(Captcha)
-admin.site.register(Currency)
+admin.site.register(Captcha, CaptchaAdmin)
+admin.site.register(Currency, CurrencyAdmin)
---
+++ models/migrations/0006_auto_20150721_0015.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('models', '0005_auto_20150720_2351'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='faucet',
+            name='category',
+            field=models.ForeignKey(to='models.FaucetCategory'),
+        ),
+    ]
