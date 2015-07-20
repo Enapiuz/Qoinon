b5f9621c9d873181755c41b95682397d5c3b97c5
Enapiuz <enapiuz@gmail.com>
2015-07-20 23:54:44 +0300

    Модель кранов, а так же сопутствующие модели

---
+++ models/migrations/0005_auto_20150720_2351.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('models', '0004_auto_20150720_2204'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FaucetCategory',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('title_en', models.CharField(max_length=300)),
+                ('title_ru', models.CharField(max_length=300)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='faucet',
+            name='reward_max',
+            field=models.FloatField(default=0),
+        ),
+        migrations.AddField(
+            model_name='faucet',
+            name='reward_mid',
+            field=models.FloatField(default=0),
+        ),
+        migrations.AddField(
+            model_name='faucet',
+            name='reward_min',
+            field=models.FloatField(default=0),
+        ),
+        migrations.AddField(
+            model_name='faucet',
+            name='views',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AlterField(
+            model_name='faucet',
+            name='update_time',
+            field=models.TimeField(help_text='Cooldown'),
+        ),
+        migrations.AddField(
+            model_name='faucet',
+            name='category',
+            field=models.ForeignKey(to='models.FaucetCategory', null=True),
+        ),
+    ]
