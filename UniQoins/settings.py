542787d041b711b9d1d49946daa99ae44a3c6f8a
Enapiuz <enapiuz@gmail.com>
2015-07-21 22:15:32 +0300

    Postgresql

--- UniQoins/settings.py
+++ UniQoins/settings.py
@@ -77,11 +77,17 @@ WSGI_APPLICATION = 'UniQoins.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+POSTGRESQL_SETTINGS = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'uniqoins',
+    'USER': 'enapiuz',
+    'PASSWORD': '',
+    'HOST': '',
+    'PORT': ''
+}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': POSTGRESQL_SETTINGS
 }
 
 
