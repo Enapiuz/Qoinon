c226661b57d8342f083e5c1fc8fcf121c18d955c
Enapiuz <enapiuz@localhost.localdomain>
2015-07-23 13:38:32 +0300

    правки файла деплоя

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,13 +2,18 @@
 
 ssh -oStrictHostKeyChecking=no root@188.166.114.242 /bin/bash << EOF
     cd /home/enapiuz/UniQoins
-    sudo -u enapiuz git pull
-    cd src
-    source ../../ucenv/bin/activate && sudo -u enapiuz pip3 install -r ../deploy/requirements.txt
-    source ../../ucenv/bin/activate && sudo -u enapiuz python manage.py migrate --noinput
-    source ../../ucenv/bin/activate && sudo -u enapiuz python manage.py bower install
-    source ../../ucenv/bin/activate && sudo -u enapiuz python manage.py collectstatic --noinput
-    cd ../
-    sudo -u enapiuz touch ./deploy/touch.me
-    echo uwsgi touch-restarted
+    exec sudo -u enapiuz /bin/bash << ENP
+        git pull
+        cd src
+        source ../../ucenv/bin/activate && pip3 install -r ../deploy/requirements.txt
+        source ../../ucenv/bin/activate && python manage.py migrate --noinput
+        source ../../ucenv/bin/activate && python manage.py bower install
+        source ../../ucenv/bin/activate && python manage.py collectstatic --noinput
+        cd ../
+        touch ./deploy/touch.me
+
+        echo uwsgi touch-restarted
+    ENP
+    sudo -u enapiuz 
+    
 EOF
