d6818778a81314053927bd0db8f21782e5f07417
Enapiuz <enapiuz@gmail.com>
2015-07-24 01:17:21 +0300

    Список капчей

--- src/front/views.py
+++ src/front/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from objects.models import Captcha
 
 
 def hello(req):
     return render(req, 'front/layout.html')
 
 def faucets(req):
-    return render(req, 'front/faucets.html')
+    captchas = Captcha.objects.all()
+    return render(req, 'front/faucets.html', {
+        'captchas': captchas
+    })
