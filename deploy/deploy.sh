746e6033ff494b51e1a8c8ec67a769ef4bd7d54a
Enapiuz <enapiuz@gmail.com>
2015-07-23 07:28:57 +0300

    Bash скрипт для деплоя

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt-get install python2
-alias python=/usr/local/bin/python2.7
-
-fab deploy
+ssh root@188.166.114.242 /bin/bash << EOF
+    cd /home/enapiuz/UniQoins
+    sudo -u enapiuz git pull
+    cd src
+    source ../../ucenv/bin/activate && sudo -u enapiuz pip3 install -r ../deploy/requirements.txt
+    source ../../ucenv/bin/activate && python manage.py migrate --noinput
+    source ../../ucenv/bin/activate && python manage.py bower install
+    source ../../ucenv/bin/activate && python manage.py collectstatic --noinput
+    cd ../
+    sudo -u enapiuz touch ./deploy/touch.me
+    echo uwsgi touch-restarted
+EOF
