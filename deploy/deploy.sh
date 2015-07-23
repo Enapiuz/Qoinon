5cec94c719401f7b94a3c6771b4673286d1e9193
Enapiuz <enapiuz@localhost.localdomain>
2015-07-23 13:32:24 +0300

    правильный пользователь для деплоя

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,9 +5,9 @@ ssh -oStrictHostKeyChecking=no root@188.166.114.242 /bin/bash << EOF
     sudo -u enapiuz git pull
     cd src
     source ../../ucenv/bin/activate && sudo -u enapiuz pip3 install -r ../deploy/requirements.txt
-    source ../../ucenv/bin/activate && python manage.py migrate --noinput
-    source ../../ucenv/bin/activate && python manage.py bower install
-    source ../../ucenv/bin/activate && python manage.py collectstatic --noinput
+    source ../../ucenv/bin/activate && sudo -u enapiuz python manage.py migrate --noinput
+    source ../../ucenv/bin/activate && sudo -u enapiuz python manage.py bower install
+    source ../../ucenv/bin/activate && sudo -u enapiuz python manage.py collectstatic --noinput
     cd ../
     sudo -u enapiuz touch ./deploy/touch.me
     echo uwsgi touch-restarted
