f4a7b8e901c579abd62c5b83bcd226b4510e267e
Enapiuz <enapiuz@gmail.com>
2015-07-26 12:17:02 +0300

    Заготовка страницы about, ссылка на эту страницу

--- src/front/urls.py
+++ src/front/urls.py
@@ -5,5 +5,5 @@ from front import views
 urlpatterns = [
     url(r'^$', views.hello, name='main'),
     url(r'^faucets/$', views.faucets, name='faucets'),
-    url(r'^faucets/about/([0-9])/$', views.faucet_about, name='faucet_about'),
+    url(r'^faucets/about/([0-9]+)/$', views.faucet_about, name='faucet_about'),
 ]
