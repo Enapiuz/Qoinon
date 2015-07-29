f4307b0d017d0b2810128dbd0bc33deb43a35acc
Enapiuz <enapiuz@gmail.com>
2015-07-30 01:29:57 +0300

    Бекап в мегу

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -10,8 +10,14 @@ sudo -u postgres pg_dump -Fc uniqoinsdb > /tmp/backup/uniqoinsdb.dump
 tar -czvf /tmp/backup/uniqoins_media.tar.gz /home/enapiuz/UniQoins/media
 tar -czvf /tmp/backup_finale/backup.tar.gz /tmp/backup
 
+rm -rf /tmp/backup_finale/backup.tar.gz.gpg
 
-megaput --username=backup@uniqoins.com --path /Root/uniqoins_backup/backup.${timestamp}.tar.gz /tmp/backup_finale/backup.tar.gz < ./megapass
+gpg --encrypt -r backup@uniqoins.com --always-trust /tmp/backup_finale/backup.tar.gz.gpg
+
+rm -rf /tmp/backup/uniqoins_media.tar.gz
+rm -rf /tmp/backup/uniqoinsdb.dump
+
+megaput --username=backup@uniqoins.com --path /Root/uniqoins_backup/backup.${timestamp}.tar.gz /tmp/backup_finale/backup.tar.gz.gpg < ./megapass
 
 
 echo "Regular backup made at ${timestamp}" | mutt -s "Regular UniQoins backup" -- enapiuz@gmail.com
