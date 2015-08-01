f87649d63be1b54acb3a8cd6c0076f1295729044
Enapiuz <enapiuz@gmail.com>
2015-08-01 13:27:53 +0300

    Попытка разрулить поддомены

--- src/UniQoins/hosts.py
+++ src/UniQoins/hosts.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django_hosts import patterns, host
 from hammer import urls as hammer_urls
 
-host_patterns = patterns('',
+host_patterns = patterns('uniqoins.com',
     host(r'', settings.ROOT_URLCONF, name='main_host'),
     host(r'hammer', hammer_urls, name='hammer_host'),
 )
