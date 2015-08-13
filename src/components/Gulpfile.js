a1b0bfa953df323b370c8e48bb06dd1f52f49a9e
Enapiuz <enapiuz@gmail.com>
2015-08-13 23:10:47 +0300

    кодировки

--- src/components/Gulpfile.js
+++ src/components/Gulpfile.js
@@ -2,31 +2,26 @@ var elixir = require('laravel-elixir');
 
 
 elixir(function(mix) {
-    // шрифты
     mix.copy(
         './bower_components/bootstrap-sass/assets/fonts',
         './static/components/fonts'
     );
 
-    // картинки
     mix.copy(
         './src/images',
         './static/components/images'
     );
 
-    // css либы
     mix.sass(
         ['../../../src/scss/vendor.scss'],
         './static/components/css/vendor.css'
     );
 
-    // css
     mix.sass(
         ['../../../src/scss/main.scss'],
         './static/components/css/main.css'
     );
 
-    // скрипты в browserify
     mix.browserify(
         '../../../src/js/main.js',
         './static/components/javascripts/main.js'
