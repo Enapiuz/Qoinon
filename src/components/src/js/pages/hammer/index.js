a1a6dc992910f0f4145296331e38dad5797d8121
Enapiuz <enapiuz@gmail.com>
2015-08-22 07:22:06 +0300

    буфер обмена

--- src/components/src/js/pages/hammer/index.js
+++ src/components/src/js/pages/hammer/index.js
@@ -2,7 +2,19 @@ module.exports = function() {
     var $ = require('jquery');
     require('jquery.cookie');
 
-    $.cookie('qwe', 'ewq');
-
     console.log('hello from hammer');
+
+    var currentCurrency = $("#hammer__current_currency_helper").val();
+    var $pastBtn = $("#hammer__past_btn");
+    var $editBtn = $("#hammer__edit_btn");
+
+    $pastBtn.click(function(){
+        var value = $.cookie('address' + currentCurrency);
+
+        if (typeof value == 'undefined') {
+
+        } else {
+
+        }
+    });
 };
--- src/components/static/components/javascripts/main.js
+++ src/components/static/components/javascripts/main.js
@@ -24217,9 +24217,17 @@ module.exports = function () {
     var $ = require('jquery');
     require('jquery.cookie');
 
-    $.cookie('qwe', 'ewq');
-
     console.log('hello from hammer');
+
+    var currentCurrency = $("#hammer__current_currency_helper").val();
+    var $pastBtn = $("#hammer__past_btn");
+    var $editBtn = $("#hammer__edit_btn");
+
+    $pastBtn.click(function () {
+        var value = $.cookie('address' + currentCurrency);
+
+        if (typeof value == 'undefined') {} else {}
+    });
 };
 
 },{"jquery":4,"jquery.cookie":3}]},{},[1]);
