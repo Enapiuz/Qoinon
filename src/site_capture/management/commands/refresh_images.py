86e5927ddf90897e09d9aba9869f7d861a77b8d6
Enapiuz <enapiuz@gmail.com>
2015-08-31 23:25:24 +0300

    починка site_capture

--- src/site_capture/management/commands/refresh_images.py
+++ src/site_capture/management/commands/refresh_images.py
@@ -20,24 +20,23 @@ class Command(BaseCommand):
 
         for faucet in faucets:
             position += 1
-            if "#" in faucet.image.name:
-                self.stdout.write('processing %s' % faucet.title_en)
-                self.stdout.write('there is {0} from {1} faucet'.format(position, faucets.count()))
-                image_name = str(uuid.uuid4()) + '.png'
-                image_path = os.path.join(faucets_images_root, image_name)
+            self.stdout.write('processing %s' % faucet.title_en)
+            self.stdout.write('there is {0} from {1} faucet'.format(position, faucets.count()))
+            image_name = str(uuid.uuid4()) + '.png'
+            image_path = os.path.join(faucets_images_root, image_name)
 
-                command = base_command.format(site=faucet.real_href(), output_file=image_path)
-                process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+            command = base_command.format(site=faucet.real_href(), output_file=image_path)
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
+            try:
+                output, errors = process.communicate(timeout=60)
+                faucet.image.name = 'faucets/%s' % image_name
+                faucet.save()
+            except Exception as e:
+                print("\t\tException: %s" % e)
                 try:
-                    output, errors = process.communicate(timeout=60)
-                    faucet.image.name = 'faucets/%s' % image_name
-                    faucet.save()
-                except Exception as e:
-                    print("\t\tException: %s" % e)
-                    try:
-                        process.kill()
-                    except Exception as oloex:
-                        print("\t\tException: %s" % oloex)
+                    process.kill()
+                except Exception as oloex:
+                    print("\t\tException: %s" % oloex)
 
         self.stdout.write('Everything done!')
