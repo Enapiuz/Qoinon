658e947c54495a6f785da9969eb10068a095ce98
Enapiuz <enapiuz@gmail.com>
2016-01-31 13:43:22 +0300

    история онлайна/офлайна крана

--- src/objects/models.py
+++ src/objects/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.validators import MinValueValidator
 from django.utils import timezone
 from django.core.cache import cache
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 import random
 from dirtyfields import DirtyFieldsMixin
 
@@ -184,5 +186,37 @@ class FaucetHistory(models.Model):
         return "{0} action at {1}".format(self.faucet.title_en, self.date)
 
 
-# попахивает тухлятинкой
-import objects.signals
+# еще немного тухлятинки
+@receiver(pre_save, sender=Faucet)
+def faucet_history_handler(sender, **kwargs):
+    # с помощью django dirty fields определить измененные поля
+    # на основе этого определить тип действия
+    # и сформировать описание
+    # записать изменения
+    # и удалить всю историю кроме последних 100 записей
+
+    history_states = {
+        'added': 0,
+        'updated': 1,
+        'removed': 2
+    }
+
+    obj = kwargs['instance']
+    if obj.is_dirty:
+        fields = obj.get_dirty_fields()
+        if len(fields) > 0:
+            if 'visible' in fields:
+                if fields['visible'] is True:
+                    FaucetHistory.objects.create(
+                            faucet=obj,
+                            action_type=history_states['updated'],
+                            action_text_ru='',
+                            action_text_en='Online'
+                    )
+                else:
+                    FaucetHistory.objects.create(
+                            faucet=obj,
+                            action_type=history_states['updated'],
+                            action_text_ru='',
+                            action_text_en='Gone offline'
+                    )
